ae4ccf52255b3c9bcf46487b2109e1c13687e529
kimminseoung01 <doboholoho@naver.com>
2025-03-05 15:16:28 +0000

    Pending changes exported from your codespace

--- server.cpp
+++ server.cpp
@@ -1,62 +1,37 @@
-#include <iostream>
-using namespace std;
+#include <windows.h>
 
-#define WIDTH 10
-#define HEIGHT 5
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-char maze[HEIGHT][WIDTH] = {
-    {'#','#','#','#','#','#','#','#','#','#'},
-    {'#','P','.','.','#','.','.','.','E','#'},
-    {'#','#','#','.','#','.','#','#','#','#'},
-    {'#','.','.','.','.','.','.','.','.','#'},
-    {'#','#','#','#','#','#','#','#','#','#'}
-};
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    WNDCLASS wc = { 0 };
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = "GameWindow";
 
-int playerX = 1, playerY = 1; // 플레이어 위치
+    RegisterClass(&wc);
 
-void displayMaze() {
-    system("cls");  // 화면 클리어
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
-            cout << maze[i][j] << " ";
-        }
-        cout << endl;
+    HWND hwnd = CreateWindow("GameWindow", "My First Windows API Game",
+        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+        NULL, NULL, hInstance, NULL);
+
+    ShowWindow(hwnd, nCmdShow);
+    MSG msg = { 0 };
+
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
+
+    return 0;
 }
 
-void movePlayer(char direction) {
-    int newX = playerX, newY = playerY;
-
-    if (direction == 'w') newX--; // 위
-    else if (direction == 's') newX++; // 아래
-    else if (direction == 'a') newY--; // 왼쪽
-    else if (direction == 'd') newY++; // 오른쪽
-
-    if (maze[newX][newY] == '.' || maze[newX][newY] == 'E') {
-        maze[playerX][playerY] = '.'; // 기존 자리 지움
-        playerX = newX;
-        playerY = newY;
-        maze[playerX][playerY] = 'P'; // 새로운 자리 이동
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
     }
-}
-
-int main() {
-    char input;
-    while (true) {
-        displayMaze();
-        cout << "WASD로 이동 (q 입력 시 종료): ";
-        cin >> input;
-
-        if (input == 'q') break;
-
-        movePlayer(input);
-
-        if (maze[playerX][playerY] == 'E') {
-            displayMaze();
-            cout << "🎉 출구에 도착! 게임 클리어!\n";
-            break;
-        }
-    }
-
     return 0;
 }
